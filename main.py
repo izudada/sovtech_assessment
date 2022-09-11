8ed10ae5512962e6e4cbd7ef37b722622fc74433
Anthony <tonyudeagbala@gmail.com>
2022-09-11 18:42:25 +0100

    Include mutations

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-from api import app, db
+from api import app
 from ariadne import load_schema_from_path, make_executable_schema, \
     graphql_sync, snake_case_fallback_resolvers, ObjectType
 from ariadne.constants import PLAYGROUND_HTML
 from flask import request, jsonify
 from api.queries import resolve_persons, resolve_person
-
-
-query = ObjectType("Query")
+from api.mutation import resolve_create_user, resolve_login_user
 
 #   Binding resolver to schema
+query = ObjectType("Query")
+
 query.set_field("persons", resolve_persons)
 query.set_field("person", resolve_person)
 
+mutation = ObjectType("Mutation")
+mutation.set_field("createUser", resolve_create_user)
+mutation.set_field("loginUser", resolve_login_user)
+
 type_defs = load_schema_from_path("schema.graphql")
 schema = make_executable_schema(
-    type_defs, query, snake_case_fallback_resolvers
+    type_defs, query, mutation, snake_case_fallback_resolvers
 )
 
 
@@ -33,8 +37,11 @@ def graphql_server():
         schema,
         data,
         context_value=request,
-        debug=app.debug
+        debug=app.debug,
     )
 
     status_code = 200 if success else 400
     return jsonify(result), status_code
+
+if  __name__ == '__main__': 
+    app.run(debug=True)
